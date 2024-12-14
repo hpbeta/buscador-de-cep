86cfc80f33c85163c25cfaab082087f4dda319ef
hpbeta <hpbeta16.l@gmail.com>
2024-12-14 18:42:46 -0300

    melhorando a indentação

--- src/home.tsx
+++ src/home.tsx
@@ -5,50 +5,64 @@ import { useState } from "react";
 import { getCep } from "./service/api";
 
 interface CepDataProps {
-  cep: number
-  logradouro: string
-  complemento: string
-  bairro: string
-  localidade: string
-  uf: string
+  cep: number;
+  logradouro: string;
+  complemento: string;
+  bairro: string;
+  localidade: string;
+  uf: string;
 }
 
 function Home() {
+  const [inputValue, setInputValue] = useState("");
+  const [cep, setCep] = useState<CepDataProps | null>(null);
 
-  const [ inputValue, setInputValue ] = useState('')
-  const [ cep, setCep ] = useState<CepDataProps | null>(null)
-
-  async function getCepData () {
-
-    if(inputValue === '') {
-      alert('Preencha o campo com um cep')
-      return
+  async function getCepData() {
+    if (inputValue === "") {
+      alert("Preencha o campo com um cep");
+      return;
     }
-    const cepData = await getCep(inputValue)
-    setCep(cepData)
-    setInputValue('')
+    const cepData = await getCep(inputValue);
+    setCep(cepData);
+    setInputValue("");
   }
- 
+
   return (
     <Container>
-        <h1>Buscador de cep</h1>
+      <h1>Buscador de cep</h1>
 
-        <ContainerCabecalho>
-          <input value={inputValue} onChange={(e) => setInputValue(e.target.value)} type="text" placeholder="Digite seu cep..." />
+      <ContainerCabecalho>
+        <input
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          type="text"
+          placeholder="Digite seu cep..."
+        />
 
-          <StyledIcons onClick={getCepData}>
-            <CiSearch />
-          </StyledIcons>
-
-        </ContainerCabecalho>
+        <StyledIcons onClick={getCepData}>
+          <CiSearch />
+        </StyledIcons>
+      </ContainerCabecalho>
 
       {cep && (
         <Main>
-          <h2><strong>Cep:</strong> {cep?.cep}</h2>
-          <span><strong>Logradouro:</strong> {cep?.logradouro}</span>
-          <span><strong>Complemento:</strong> {cep?.complemento ? cep.complemento : 'Sem complemento'}</span>
-          <span><strong>Bairro:</strong> {cep?.bairro}</span>
-          <span><strong>Localidade: </strong>{cep?.localidade} - {cep?.uf}</span>
+          <h2>
+            <strong>Cep:</strong> {cep?.cep}
+          </h2>
+          <span>
+            <strong>Logradouro:</strong> {cep?.logradouro}
+          </span>
+          <span>
+            <strong>Complemento:</strong>{" "}
+            {cep?.complemento ? cep.complemento : "Sem complemento"}
+          </span>
+          <span>
+            <strong>Bairro:</strong> {cep?.bairro}
+          </span>
+          <span>
+            <strong>Localidade: </strong>
+            {cep?.localidade} - {cep?.uf}
+          </span>
         </Main>
       )}
     </Container>
--- src/service/api.ts
+++ src/service/api.ts
@@ -1,10 +1,12 @@
 import axios from "axios";
 
-export async function getCep (inputValue: string) {
-    try {
-        const response = await axios.get(`https://viacep.com.br/ws/${inputValue}/json/`)
-        return response.data
-    } catch {
-        console.log('Erro ao buscar o cep')
-    }
+export async function getCep(inputValue: string) {
+  try {
+    const response = await axios.get(
+      `https://viacep.com.br/ws/${inputValue}/json/`
+    );
+    return response.data;
+  } catch {
+    console.log("Erro ao buscar o cep");
+  }
 }
